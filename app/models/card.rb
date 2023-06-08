5d21e34f7605c9240ea8fcf73838b121f01143c7
Andres Munoz <andremude@hotmail.com>
2023-06-08 08:29:13 -0500

    add validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,5 @@
 class Card < ApplicationRecord
   belongs_to :deck
+  validates :question, presence: true
+  validates :answer, presence: true
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,5 @@
 class Deck < ApplicationRecord
   has_many :cards, dependent: :destroy
   belongs_to :user
+  validates :name, presence: true
 end
